d19fe1b48b1d3c927dffaad7d546a77c677f1cfb
Sean Hoe Kai Zher <102246906+Seann2003@users.noreply.github.com>
2025-04-03 05:11:10 +0800

    Commit again for class 1 and class 2

--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -1,6 +1,11 @@
-import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
-import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import {
+  Keypair,
+  PublicKey,
+  Connection,
+  type Commitment,
+} from "@solana/web3.js";
+import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
+import wallet from "../dev-wallet.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -12,18 +17,33 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 const token_decimals = 1_000_000n;
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("CJSyZM2fQGSGa9hMqAdUeki6iS2Ubowtzi8WoU9LCz8d");
 
 (async () => {
-    try {
-        // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
-
-        // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+  try {
+    // Create an ATA
+    const ata = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
+    console.log(`Your ata is: ${ata.address.toBase58()}`);
+    // Mint to ATA
+    const mintTx = await mintTo(
+      connection,
+      keypair,
+      mint,
+      ata.address,
+      keypair.publicKey,
+      token_decimals
+    );
+    console.log(
+      `https://explorer.solana.com/transaction/${mintTx}?cluster=devnet`
+    );
+    console.log(`Your mint txid: ${mintTx}`);
+    return mint;
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
