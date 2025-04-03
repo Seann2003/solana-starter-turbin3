b4b1441c62d8e6af18af4de1da917a22f29265a5
Sean Hoe Kai Zher <102246906+Seann2003@users.noreply.github.com>
2025-04-04 05:35:36 +0800

    minted nft

--- ts/cluster1/nft_metadata.ts
+++ ts/cluster1/nft_metadata.ts
@@ -1,10 +1,14 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
+import wallet from "../dev-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
 
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
@@ -13,33 +17,36 @@ umi.use(irysUploader());
 umi.use(signerIdentity(signer));
 
 (async () => {
-    try {
-        // Follow this JSON structure
-        // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+  try {
+    // Follow this JSON structure
+    // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your metadata URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+    const image =
+      "https://devnet.irys.xyz/H6Y2bRbWxcB6DdtwGKJ2x6jP8ZFrN8tcgrT5qrhZ53iR";
+    const metadata = {
+      name: "Rug",
+      symbol: "RUG",
+      description: "A rug on Solana",
+      image: image,
+      attributes: [
+        { trait_type: "Colour", value: "white" },
+        { trait_type: "Size", value: "small" },
+        { trait_type: "Material", value: "cotton" },
+      ],
+      properties: {
+        files: [
+          {
+            type: "image/png",
+            uri: image,
+          },
+        ],
+      },
+      creators: [],
+    };
+
+    const myUri = await umi.uploader.uploadJson(metadata);
+    console.log("Your metadata URI: ", myUri);
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
 })();
